b7aa284c3dc9e067eccf575aa53adfeb4a9502a4
Chad Gilbert <chad.gilbert@cqlcorp.com>
2017-08-29 13:01:59 -0400

    Move envars to Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -5,6 +5,10 @@ MAINTAINER Chad Gilbert <chad.gilbert@cqlcorp.com>
 
 ARG JENKINS_SWARM_CLIENT_VERSION=3.4
 
+ENV LABELS=docker EXECUTORS=3 FSROOT=C:\tmp\jenkins NODENAMEPREFIX=docker-
+
+VOLUME C:\tmp
+
 RUN powershell -Command Invoke-WebRequest $('https://repo.jenkins-ci.org/releases/org/jenkins-ci/plugins/swarm-client/' + $Env:JENKINS_SWARM_CLIENT_VERSION + '/swarm-client-' + $Env:JENKINS_SWARM_CLIENT_VERSION + '.jar') -OutFile 'swarm-client.jar' -UseBasicParsing ;
 
 COPY entrypoint.cmd C:\entrypoint.cmd
--- entrypoint.cmd
+++ entrypoint.cmd
@@ -3,25 +3,5 @@ SETLOCAL EnableDelayedExpansion
 
 SET /p JENKINS_SECRET=<C:\ProgramData\Docker\secrets\jenkins
 
-IF NOT DEFINED LABELS (
-    SET LABELS=docker
-)
-
-IF NOT DEFINED EXECUTORS (
-    SET EXECUTORS=3
-)
-
-IF NOT DEFINED FSROOT (
-    SET FSROOT=C:\tmp\jenkins
-)
-
-IF NOT DEFINED NODENAMEPREFIX (
-    SET NODENAMEPREFIX=docker-
-)
-
-IF NOT EXIST "%FSROOT%" (
-    mkdir "%FSROOT%"
-)
-
 cmd /c java -jar swarm-client.jar -labels "%LABELS%" -executors "%EXECUTORS%" -fsroot "%FSROOT%" -name %NODENAMEPREFIX%%COMPUTERNAME% %JENKINS_SECRET% || exit /b
 
