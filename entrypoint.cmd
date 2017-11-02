261b2979c9cd9696e031d4378b061760b99b3926
Chad Gilbert <chad.gilbert@cqlcorp.com>
2017-11-02 07:45:58 -0400

    Improve secret and environment variable handling

--- entrypoint.cmd
+++ entrypoint.cmd
@@ -1,7 +1,26 @@
 @ECHO OFF
 SETLOCAL EnableDelayedExpansion
 
-SET /p JENKINS_SECRET=<C:\ProgramData\Docker\secrets\jenkins
 
-cmd /c java -jar swarm-client.jar -labels "%LABELS%" -executors "%EXECUTORS%" -fsroot "%FSROOT%" -name %NODENAMEPREFIX%%COMPUTERNAME% %JENKINS_SECRET% || exit /b
+IF NOT DEFINED LABELS (
+    SET LABELS=docker
+)
+
+IF NOT DEFINED EXECUTORS (
+    SET EXECUTORS=3
+)
+
+IF NOT DEFINED FSROOT (
+    SET FSROOT=C:\w
+)
+
+IF NOT DEFINED NODENAMEPREFIX (
+    SET NODENAMEPREFIX=docker-
+)
+
+IF NOT DEFINED JENKINS_PASSWORD (
+    SET /p JENKINS_PASSWORD=< C:\ProgramData\Docker\secrets\jenkins-agent-password
+)
+
+cmd /c java -jar swarm-client.jar -labels "%LABELS%" -executors "%EXECUTORS%" -fsroot "%FSROOT%" -name "%NODENAMEPREFIX%%COMPUTERNAME%" -disableClientsUniqueId -master "%JENKINS_URL%" -sslFingerprints "%JENKINS_SSL_FINGERPRINTS%" -username "%JENKINS_USERNAME%" -passwordEnvVariable JENKINS_PASSWORD || exit 1 /b
 
